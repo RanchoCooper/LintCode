8d4da53ef4673ed2fcc23239980acbbe5fac7bb2
RanchoCooper <1006959160@qq.com>
2016-05-13 00:26:35 +0800

    notes

--- kth-largest-element.py
+++ kth-largest-element.py
@@ -7,9 +7,30 @@
 # In array [1,2,3,4,5], the 1st largest element is 5, 2nd largest element is 4,
 # 3rd largest element is 3 and etc.
 
-class Solutin:
+class Solution:
     # @param k & A a integer and an array
     # @return ans a integer
+    def partition(self, A, left, right):
+        pivot = A[left]
+        l = left + 1
+        r = right
+        while l <= r:
+            if A[l] < pivot and pivot > A[r]:
+                A[l], A[r] = A[r], A[l]
+            if pivot <= A[l]:
+                l += 1
+            if pivot >= A[r]:
+                r -= 1
+        A[left], A[r] = A[r], A[left]
+        return r
     def kthLargestElement(self, k, A):
-        A = sorted(A, reverse=True)
-        return A[k-1]
+        # A = sorted(A, reverse=True)
+        left, right = 0, len(A) - 1
+        while True:
+            pos = self.partition(A, left, right)
+            if pos == k -1:
+                return A[pos]
+            elif pos < k -1:
+                left = pos + 1
+            else:
+                right = pos - 1
