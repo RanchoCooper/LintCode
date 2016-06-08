61fa992b242f29684e6477c1872103b9eb419f4f
RanchoCooper <1006959160@qq.com>
2016-06-09 01:55:23 +0800

    test ubuntu

---
+++ 4sum.cpp
@@ -0,0 +1,24 @@
+/*
+ * @author: Rancho (rancho941110@gmail.com)
+ * @date  : 2016-05-05 14:20:14
+ *
+ * Brief:
+ * Given an array S of integers, find four elements a, b, c and d in S
+ * such that a + b + c + d = target. notice a, b, c, d
+ * must be non-descending order (ie, a ≤ b ≤ c ≤ d)
+ * Example:
+ * Given array S = {1 0 -1 0 -2 2}, and target = 0. A solution set is:
+ *     (-1, 0, 0, 1)
+ *     (-2, -1, 1, 2)
+ *     (-2, 0, 0, 2)
+ * Detail:
+ */
+#pragma once
+#include <iostream>
+
+
+
+/*
+ * Reflection:
+ *
+ */
